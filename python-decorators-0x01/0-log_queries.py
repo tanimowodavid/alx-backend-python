7661ee4a4661aac9eb3de1549fdfe0a1b9549510
David <davidtanimowo01@gmail.com>
2025-11-07 05:54:58 +0100

    fix:timestamp added to log query

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,16 +1,20 @@
 import sqlite3
 import functools
+from datetime import datetime
 
 #### decorator to log SQL queries
 """ your code goes here"""
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        query = kwargs.get('query', '') if 'query' in kwargs else args[0] if args else ''
-        print(f"Executing SQL Query: {query}")
+        query = args[0] if args else kwargs.get('query', '')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"[{timestamp}] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
+
+#### function to fetch all users from the database with the decorator applied
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
