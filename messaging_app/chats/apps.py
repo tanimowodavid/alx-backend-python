c6664a7e317583f3f2108d4097175129793a75e4
David <davidtanimowo01@gmail.com>
2025-11-22 14:39:47 +0100

    week 4 fix

--- messaging_app/chats/apps.py
+++ messaging_app/chats/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging_app.chats'
+    name = 'chats'
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'messaging_app.chats.apps.ChatsConfig',
+    'chats.apps.ChatsConfig',
     'rest_framework',
 ]
 
